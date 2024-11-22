841b9c4aac31048457e39afcf51a56b433dbc1e3
Maliheh <mkareshky@yahoo.com>
2024-11-22 10:10:05 +0100

    chore: hide api key

--- src/utils/fetchWeather.ts
+++ src/utils/fetchWeather.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '85d61276322313555bba7e04eedbbead'; // Replace with your OpenWeatherMap API key
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export const fetchWeather = async (city: string) => {
