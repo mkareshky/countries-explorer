e4ce55399ef971f9685bc690c0358c1f458013be
Maliheh <mkareshky@yahoo.com>
2024-11-23 13:41:41 +0100

    test: fix

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-    transform: {
-      "^.+\\.tsx?$": "babel-jest",
-      "^.+\\.jsx?$": "babel-jest"
-    },
-    transformIgnorePatterns: [
-      "/node_modules/(?!(axios)/)" // Transforms ES modules like axios
-    ],
-  };
-  
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest', // Use Babel to transform TypeScript and JavaScript files
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!axios)/', // Allow Jest to transform `axios` or any other ESM packages
+  ],
+  moduleNameMapper: {
+    '\\.(css|scss|sass)$': 'identity-obj-proxy', // Mock CSS imports
+  },
+  testEnvironment: 'jsdom', // Use jsdom for browser-like environment
+};
